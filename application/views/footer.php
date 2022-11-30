32b8a2d4ef57d3a54dfacd339072f64187a19c9b
JavierJhon <jhonatan.lasso5729@utc.edu.ec>
2022-11-30 00:49:09 -0500

    inicio
    Co-Authored-By: null <JavierJhon@users.noreply.github.com>

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,6 +3,9 @@
 <br><br><br><br><br><br><br><br><br><br><br><br><br>
 <div class="well text-center">
   <h4><b><i class="glyphicon glyphicon-user"></i>REALIZADO POR:JAVIER LASSO Y ANDRES BAUTISTA</b></h4>
+
+<br>
+<br><b></b>
 </div>
   </body>
 </html>
